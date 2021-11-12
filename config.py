2ddd3a0d31e049171b58215b8ee1a0d3e90c7367
RuiyeCheng <90012570+RuiyeCheng@users.noreply.github.com>
2021-11-11 19:30:31 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 dbparams = {
     'host': 'database-2.coqj7xy0vhvp.us-west-2.rds.amazonaws.com',
     'username': 'admin',
-    'password': 'password',
+    'password': 'besttrade_d',
     'database': 'besttrade_d'
 }
